6ae77c8e78aebf586bfd6e5a4c6fb9093368dea5
liubiqiang666 <1741120849@qq.com>
2019-05-28 17:24:21 +0800

    1

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li @click="handdelete">{{content}}</li>
+</template>
+
+<script>
+	export default{
+		props:['content','index'],
+		methods:{
+			handdelete:function(){
+				this.$emit('delete',this.index)
+			}
+		}
+	}
+</script>
+
+
+<style scoped>
+
+</style>
